90c7f52445462bbeacbcb5eca88af6bc942b70e0
Patrick Brouwer <patrick@patrickbrouwer.eu>
2020-03-06 08:41:26 +0100

    First commit

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../App.css";
+
+function Header() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="../winc.png" alt="" />
+        <h1> Lil' Playlist</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
