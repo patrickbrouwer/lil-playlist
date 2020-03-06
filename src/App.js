c5d3c1b521f62a69d3b7a0006dfd1691285812c7
Patrick Brouwer <patrick@patrickbrouwer.eu>
2020-03-06 08:50:06 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
+import SongOverview from "./components/SongOverview";
 
 function App() {
   return <Header />;
+  <SongOverview />;
 }
 
 export default App;
